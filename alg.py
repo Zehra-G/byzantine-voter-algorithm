01bda28ea8218d24da97055823fddecd5257aca3
zgundo25@colby.edu <zehragundogdu@Zehras-MacBook-Air.local>
2023-11-20 17:03:19 -0500

    initial commit

---
+++ alg.py
@@ -0,0 +1,12 @@
+'''
+This is an initial implementation of the Byzantine voter resistant ranking profile algorithm of:
+
+Melnyk, Darya, Yuyi Wang, and Roger Wattenhofer. "Byzantine preferential voting."
+International Conference on Web and Internet Economics. Cham: Springer International
+Publishing, 2018.
+
+Zehra Gundogdu & Colin Huh
+CS381 FA23 Final Project
+Check In 1
+
+'''
