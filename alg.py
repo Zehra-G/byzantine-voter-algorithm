9ee9ad705243bce67a3341ddec07b1f7ccc1c264
zgundo25@colby.edu <zehragundogdu@Zehras-MacBook-Air.local>
2023-11-20 17:38:15 -0500

    added initial implementation

--- alg.py
+++ alg.py
@@ -11,6 +11,96 @@ Check In 1
 
 '''
 
+from collections import defaultdict
+import random
 
-print("hello")
+class Node:
+    def __init__(self, node_id, total_nodes, rankings):
+        self.node_id = node_id
+        self.total_nodes = total_nodes
+        self.rankings = rankings
+        self.fixed_pairs = set()
 
+    def broadcast_ranking(self):
+        return self.rankings
+
+    def propose_pairs(self, rankings):
+        proposals = set()
+        for i in range(len(rankings)):
+            for j in range(i + 1, len(rankings)):
+                ci, cj = rankings[i], rankings[j]
+                if self.count_rankings_above(ci, cj) >= self.total_nodes - self.threshold:
+                    proposals.add((ci, cj))
+        return proposals
+
+    def count_rankings_above(self, ci, cj):
+        count = 0
+        for ranking in self.rankings:
+            if ranking.index(ci) < ranking.index(cj):
+                count += 1
+        return count
+
+    def receive_proposal(self, proposals):
+        received_proposals = defaultdict(int)
+        for proposal in proposals:
+            received_proposals[proposal] += 1
+
+        for proposal, count in received_proposals.items():
+            if count >= self.threshold + 1:
+                self.fix_pair(proposal)
+
+    def fix_pair(self, pair):
+        self.fixed_pairs.add(pair)
+        # Adjust own ranking to satisfy fixed pairs (not implemented in this example)
+
+    def broadcast_fixed_pairs(self):
+        return self.fixed_pairs
+
+    def dictator_phase(self, dictator_ranking):
+        # Dictator suggests its own ranking during Dictator Phase
+        if self.is_correct_node():
+            self.rankings = dictator_ranking
+
+    def is_correct_node(self):
+        # Simulating a check for correctness (replace with actual implementation)
+        return random.choice([True, False])
+
+    def decision_phase(self, dictator_ranking):
+        if all(pair in self.fixed_pairs for pair in dictator_ranking):
+            self.rankings = dictator_ranking
+
+def consensus_algorithm(nodes, rounds):
+    for round_number in range(rounds):
+        # Select t + 1 different nodes as dictators for the round
+        round_dictators = random.sample(nodes, len(nodes[0].fixed_pairs) + 1)
+
+        for node in nodes:
+            # Communication Phase
+            node.broadcast_ranking()
+            proposals = node.propose_pairs(node.rankings)
+            node.receive_proposal(proposals)
+
+            # Dictator Phase
+            dictator_ranking = round_dictators[round_number].broadcast_ranking()
+            node.dictator_phase(dictator_ranking)
+
+            # Decision Phase
+            node.decision_phase(dictator_ranking)
+
+    return [node.rankings for node in nodes]
+
+# Example usage:
+total_nodes = 4
+threshold = 1  # Adjust as needed
+rounds = 5
+
+nodes = [Node(node_id=i, total_nodes=total_nodes, rankings=['A', 'B', 'C']) for i in range(total_nodes)]
+
+for node in nodes:
+    node.threshold = threshold
+
+consensus_rankings = consensus_algorithm(nodes, rounds)
+
+print("Consensus Rankings:")
+for i, rankings in enumerate(consensus_rankings):
+    print(f"Node {i}: {rankings}")
